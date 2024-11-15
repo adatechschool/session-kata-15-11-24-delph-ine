515b47250435204620e541c94e17f17976087e5c
delphine <o.df@free.fr>
2024-11-15 12:32:22 +0100

    cata le morse

---
+++ script.js
@@ -0,0 +1,78 @@
+//j'écris ma fonction qui prend en paramètre
+//du texte et retourne un tableau contenant chaque
+//caractère
+
+//je crée un tableau de lettre
+let lettre = ["H", "e", "l", "l", "o", ",", " ", "w", "o", "r", "l", "d"]
+//console.log(lettre)
+
+//je crée ma fonction
+function getLatinCharacterList(){
+
+    const latinToMorse = {
+        'A':'.-',
+        'B':'-...',
+        'C':'-.-.',
+        'D':'-..',
+        'E':'.',
+        'F':'..-.',
+        'G':'--.',
+        'H':'....',
+        'I':'..',
+        'J':'.---',
+        'K':'-.-',
+        'L':'.-..',
+        'M':'--',
+        'N':'-.',
+        'O':'---',
+        'P':'.--.',
+        'Q':'--.-',
+        'R':'.-.',
+        'S':'...',
+        'T':'-',
+        'U':'..-',
+        'V':'...-',
+        'W':'.--',
+        'X':'-..-',
+        'Y':'-.--',
+        'Z':'--..'
+    }
+    const morseToLatin = {
+        '-': "T",
+        '--': "M",
+        '---': "O",
+        '--.': "G",
+        '--.-': "Q",
+        '--..': "Z",
+        '-.': "N",
+        '-.-': "K",
+        '-.--': "Y",
+        '-.-.': "C",
+        '-..': "D",
+        '-..-': "X",
+        '-...': "B",
+        '.': "E",
+        '.-': "A",
+        '.--': "W",
+        '.---': "J",
+        '.--.': "P",
+        '.-.': "R",
+        '.-..': "L",
+        '..': "I",
+        '..-': "U",
+        '..-.': "F",
+        '...': "S",
+        '...-': "V",
+        '....': "H"
+      }
+    let morseCode = "";
+    }
+//console.log(getLatinCharacterList)
+
+for(let index = 
+    ; index<lettre.length; index++){
+
+}
+//j'appelle ma fonction
+getLatinCharacterList()
+//console.log(getLatinCharacterList)
